88c1404b8d5ad86e49a159be565c838dbded481a
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-01-15 14:30:11 +0530

    Added rrouting

--- 05form/src/App.jsx
+++ 05form/src/App.jsx
@@ -1,21 +1,27 @@
 import Signup from "./components/Signup"
 import './App.css';
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Products from "./components/Products";
+import Addproduct from "./components/Addproduct";
 function App() {
 
   return (
     <>
-    
+<Navbar/>
 <Router>
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/login" element ={<Login/>} />
         <Route path="/signup" element={<Signup/>} />
+        <Route path="/products" element={<Products/>} />
+        {/* <Route path="/add-product" element={<Addproduct/>} /> */}
       </Routes>
 </Router>
 
-  </>
+ 
+ </>
   )
 }
 
