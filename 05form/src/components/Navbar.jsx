1f560294b73a5df8fb9be3863bc045a84c099e19
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-01-16 12:26:14 +0530

    Category

--- 05form/src/components/Navbar.jsx
+++ 05form/src/components/Navbar.jsx
@@ -4,12 +4,23 @@ function Navbar() {
 
     const [isMenuOpen,setIsMenuOpen] = useState(false);
 
+    const [isCategoryOpen,setIsCategoryOpen] = useState(false);
+
     const toggleMenu = () => {
 
         setIsMenuOpen(!isMenuOpen) // !false = true
 
     }
     // console.log(isMenuOpen)
+
+    const handleCategoryMouseEnter = () => {
+      setIsCategoryOpen(true);
+  };
+
+  const handleCategoryMouseLeave = () => {
+      setIsCategoryOpen(false);
+  };
+
 
   return (
     <>
@@ -42,8 +53,28 @@ function Navbar() {
             <li><a href="#" className='text-white'>Home</a></li>
             <li><a href="#" className='text-white'>About</a></li>
             <li><a href="/Products" className='text-white'>Products</a></li>
-            <li><a href="#" className='text-white'>Order</a></li>
-            <li><a href="#" className='text-white'>Contact</a></li>
+            <li><a href="/order" className='text-white'>Order</a></li>
+
+
+            <li className='relative' onMouseEnter={handleCategoryMouseEnter}  onMouseLeave={handleCategoryMouseLeave}>
+              <a href="#" className='text-white'>Categories</a>
+
+              {isCategoryOpen && (
+                                <ul className="absolute top-full left-0 bg-white text-black shadow-md rounded-md">
+                                    <li className="px-4 py-2 hover:bg-gray-200">
+                                        <a href="#">Mens</a>
+                                    </li>
+                                    <li className="px-4 py-2 hover:bg-gray-200">
+                                        <a href="#">Womens</a>
+                                    </li>
+                                    <li className="px-4 py-2 hover:bg-gray-200">
+                                        <a href="#">Kids</a>
+                                    </li>
+                                   </ul>
+              )}
+
+            </li>
+              
             <li><a href="/Login" className='text-white'>Login</a></li>
            </ul>
         </div>
@@ -53,7 +84,29 @@ function Navbar() {
             <li className='py-2'><a href="#" className='text-white'>Home</a></li>
             <li className='py-2'><a href="#" className='text-white'>About</a></li>
             <li className='py-2'><a href="/Products" className='text-white'>Products</a></li>
-            <li className='py-2'><a href="#" className='text-white'>Order</a></li>
+            <li className='py-2'><a href="/order" className='text-white'>Order</a></li>
+
+            <li className='py-2 relative' onMouseEnter={handleCategoryMouseEnter} onMouseLeave={handleCategoryMouseLeave}>
+
+              <a href="#" className='text-white'>Categories</a>
+              
+              {isCategoryOpen && (
+                 <ul className="absolute top-full left-0 bg-white text-black shadow-md rounded-md">
+                 <li className="px-4 py-2 hover:bg-gray-200">
+                     <a href="#">Mens</a>
+                 </li>
+                 <li className="px-4 py-2 hover:bg-gray-200">
+                     <a href="#">Womens</a>
+                 </li>
+                 <li className="px-4 py-2 hover:bg-gray-200">
+                     <a href="#">Kids</a>
+                 </li>
+                </ul>
+              )}
+            
+            </li>
+
+
             <li className='py-2'><a href="/Login" className='text-white'>Login</a></li>
            </ul>) : null} 
       </nav>
