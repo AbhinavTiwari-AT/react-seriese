47d0cc0f84d23399ae059344a2f7bde10d8f15ff
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-01-24 11:36:57 +0530

    Axios

---
+++ axiosApi/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Product from './Components/Product'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <Navbar/>
+      <Product/>
+      <Footer/>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ axiosApi/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+function Footer() {
+  return (
+    <div className='foooter'>
+        <p>Shopping App 2025 &copy;</p>
+    </div>
+  )
+}
+
+export default Footer
